b1c3652ea7f2628e206da289388521ab51f5baa2
Mia-jeong <miajeongdev@gmail.com>
2019-11-15 22:42:45 +0900

    eatgo controller test initial setup

--- eatgo/eatgo-api/src/main/java/com/mia/eatgo/domain/Restaurant.java
+++ eatgo/eatgo-api/src/main/java/com/mia/eatgo/domain/Restaurant.java
@@ -4,6 +4,7 @@ public class Restaurant {
 
     private  String name;
     private String address;
+    private String id;
 
     public Restaurant(String name) {
         this.name = name;
@@ -14,8 +15,23 @@ public class Restaurant {
         this.address = address;
     }
 
+    public Restaurant(String name, String address, String id) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+
+
     public String  getName() {
         return this.name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public Object getInformation() {
---
+++ eatgo/eatgo-api/src/main/java/com/mia/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,20 @@
+package com.mia.eatgo.interfaces;
+
+import com.mia.eatgo.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list(){
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant("mia", "Vancouver", "m01");
+        restaurants.add(restaurant);
+        return restaurants;
+    }
+}
